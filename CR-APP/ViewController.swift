7bc10f35eb2fefd2cbaecc8ef815c42032c7cf3a
Saúl Ponce <saul.ponce@wizeline.com>
2017-07-06 10:37:04 -0500

    Add ability to connect to the API

--- CR-APP/ViewController.swift
+++ CR-APP/ViewController.swift
@@ -7,18 +7,20 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController, SwiftyDrawViewDelegate {
     
+    @IBOutlet weak var recognizedText: UITextField!
+    @IBOutlet weak var drawingView: UIView!
     var timer = Timer()
-    var image: UIImage? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         let viewToDraw = SwiftyDrawView(frame: self.view.frame)
-        self.view.addSubview(viewToDraw)
+        self.drawingView.addSubview(viewToDraw)
         
         viewToDraw.delegate = self
     }
@@ -26,12 +28,6 @@ class ViewController: UIViewController, SwiftyDrawViewDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let destination = segue.destination as! ImageViewController
-        destination.imageToShow = self.image
-        
     }
     
     func imageWithView (view: UIView) -> UIImage {
@@ -52,33 +48,68 @@ class ViewController: UIViewController, SwiftyDrawViewDelegate {
     }
     
     func clearCanvas() {
-        self.image = self.imageWithView(view: self.view)
-        let canvas = self.view.subviews[2] as! SwiftyDrawView
+        getLetterFromImage()
+        
+        let canvas = self.drawingView.subviews.first! as! SwiftyDrawView
         canvas.clearCanvas()
-        print("called")
+    }
+    
+    func getLetterFromImage() {
+        let image = self.imageWithView(view: self.drawingView)
+        let imageData = UIImagePNGRepresentation(image)
+        let base64Image = imageData!.base64EncodedString(options: .lineLength64Characters)
+        requestWithImage(base64Image: base64Image)
+    }
+    
+    func requestWithImage(base64Image: String) {
+        let headers = [
+            "authorization": "Basic ZE9kYXFpOk4qZ2hAYm41NDIk",
+            "content-type": "application/json",
+            "cache-control": "no-cache",
+            "postman-token": "b00f5c78-31f8-e906-cedb-e032df54f80e"
+        ]
+        let parameters = ["image": base64Image]
+        
+        let postData = try? JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "https://a0bc4589.ngrok.io/api")! as URL)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = headers
+        request.httpBody = postData
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print("\(error!)")
+            } else {
+                let httpResponse = response as? HTTPURLResponse
+                print("\(httpResponse!)")
+            }
+        })
+        
+        dataTask.resume()
+    }
+    
+    @IBAction func tap(_ sender: Any) {
+        UIPasteboard.general.string = self.recognizedText.text
     }
     
     func SwiftyDrawDidBeginDrawing(view: SwiftyDrawView) {
         // Called when the SwiftyDrawView detects touches have begun.
-        
     }
     
     func SwiftyDrawIsDrawing(view: SwiftyDrawView) {
         // Called when the SwiftyDrawView detects touches are currrently occuring.
         // Will be called multiple times.
-        
     }
     
     func SwiftyDrawDidFinishDrawing(view: SwiftyDrawView) {
         self.scheduledTimerWithInterval()
         // Called when the SwiftyDrawView detects touches have ended for the particular line segment
-        
     }
     
     func SwiftyDrawDidCancelDrawing(view: SwiftyDrawView) {
-        
         // Called if SwiftyDrawView detects issues with the gesture recognizers and cancels the drawing
-        
     }
 }
 
